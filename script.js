13e77dfc17bf410079931b9eb73c59b91c57431c
ldmpz <113418204+ldmpz@users.noreply.github.com>
2024-03-18 02:51:12 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+document.getElementById('btnDescargar').addEventListener('click', function() {
+    // Aquí especificas la URL del archivo que quieres descargar
+    var url = '/img/curriculum.pdf';
+    
+    // Crear un enlace temporal
+    var link = document.createElement('a');
+    link.href = url;
+    link.download = 'CV_LeonardoDaniel_Martinez_Perez'; // Puedes establecer un nombre para el archivo aquí
+    
+    // Hacer clic en el enlace
+    document.body.appendChild(link);
+    link.click();
+    
+    // Limpiar el enlace temporal
+    document.body.removeChild(link);
+});
+
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+//Funcion que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("photoshop");
+        habilidades[3].classList.add("wordpress");
+        habilidades[4].classList.add("drupal");
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajo");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("dedicacion");
+        habilidades[9].classList.add("proyect");
+    }
+}
+
+
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+} 
